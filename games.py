d1dfd6f02d2898229f08fc6bb7f2522bffec288d
Cécile <cchambeau@protonmail.com>
2020-04-25 11:39:40 -0400

    Add rule that bet cannot be null, add intro to game, ass first flip result

--- games.py
+++ games.py
@@ -2,7 +2,20 @@ import random
 
 money = 100
 
-#Write your game of chance functions here
+def flip_coin(call, bet):
+    #Has to bet money
+    if bet <= 0:
+        print("They say you need money to make money! Make sure your bet is over 0.")
+        return 0
+    
+    #Intro to game -> Prints player's call
+    print("Let's flip a coin! You called " + call)
+    result = random.int(1,2)
+
+    #Prints flip result -> Heads=1 Tails=2
+    if result == 1:
+        print("Heads!")
+    else: print("Tails!")
 
 
 
