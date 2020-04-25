2bb2f8ea80aca7098139f13b2ea5cfa5b5189319
Cécile <cchambeau@protonmail.com>
2020-04-25 12:15:32 -0400

    Add Cho-Han game of dice

--- games.py
+++ games.py
@@ -26,6 +26,30 @@ def flip_coin(call, bet):
         return -bet
 
 
+def cho_han(call, bet):
+    #Has to bet money
+    if bet <= 0:
+        print("They say you need money to make money! Make sure your bet is over 0.")
+        return 0
+
+    print("Let's play Cho-Han!")
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    total = dice1 + dice2
+    print("The sum of the dice is " + str(total))
+
+    if call == "Even" and total % 2 == 0:
+        print("You've won " + str(bet)+" dollars!")
+        print("------------------")
+        return bet
+    elif call == "Odd" and total % 2 == 1:
+        print("You've won " + str(bet)+" dollars!")
+        print("------------------")
+        return bet
+    else:
+        print("You've lost " + str(bet)+" dollars!")
+        print("------------------")
+        return -bet
 
 
 #Call your game of chance functions here
